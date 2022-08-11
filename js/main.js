976c30e91284eab7fd8e248629d1960445f536f8
longbui-novobi <long.bui@novobi.com>
2022-08-11 16:06:25 +0700

    REL

--- js/main.js
+++ js/main.js
@@ -171,6 +171,9 @@ class Main extends Component {
     }
     issueNavigator() {
         let self = this;
+        if (this.issueData){
+            this.reloadIssueRef.el.style.visibility = 'visible';
+        }
         this.openIssueref.el.addEventListener('click', this.openIssueNaviagor)
         this.reloadIssueRef.el.addEventListener('click', (event) => {
             self.fetchIssueFromServer();
@@ -198,15 +201,12 @@ class Main extends Component {
             if (record.active_duration > 0) {
                 this.issueData.timeStatus = "pause";
             }
-            else if (record.last_start) {
+            if (record.last_start) {
                 let pivotTime = new Date().getTime();
                 this.currentInterval = setInterval(() => {
                     this.activeDurationRef.el.innerText =this.secondToString(parseInt(record.active_duration + (new Date().getTime() - pivotTime) / 1000));
                 }, 500)
                 this.issueData.timeStatus = "active";
-            } 
-            else {
-                this.issueData.timeStatus = "none";
             }
         } 
         this.renderTimeActions();
@@ -1406,6 +1406,11 @@ class Main extends Component {
         return res;
     }
     template = `<div class="main-action-page show">
+        <div class="reload-issue" l-ref="reload-issue" tabindex="999" title="Reload From The Original Server">
+            <span class="tm-icon-svg">
+                <svg class="svg-inline--fa fa-arrow-rotate-right" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-rotate-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M496 48V192c0 17.69-14.31 32-32 32H320c-17.69 0-32-14.31-32-32s14.31-32 32-32h63.39c-29.97-39.7-77.25-63.78-127.6-63.78C167.7 96.22 96 167.9 96 256s71.69 159.8 159.8 159.8c34.88 0 68.03-11.03 95.88-31.94c14.22-10.53 34.22-7.75 44.81 6.375c10.59 14.16 7.75 34.22-6.375 44.81c-39.03 29.28-85.36 44.86-134.2 44.86C132.5 479.9 32 379.4 32 256s100.5-223.9 223.9-223.9c69.15 0 134 32.47 176.1 86.12V48c0-17.69 14.31-32 32-32S496 30.31 496 48z"></path></svg>                        
+            </span>
+        </div>
         <div class="issue search-bar">
             <div class="input-group justify-content-between">
                 <span class="icon-prepend" l-ref="reload-issue" tabindex="999" title="Reload From The Original Server">
