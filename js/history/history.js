908654145506d2db6b9b6d645b1f5fdb3fc09539
longbui-novobi <long.bui@novobi.com>
2023-02-09 18:33:07 +0700

    REL

--- js/history/history.js
+++ js/history/history.js
@@ -2,6 +2,7 @@
 import * as util from "../utils/utils.js"
 import { Component } from "../base.js"
 import { LogByDate } from "./historyByDate.js"
+import { Favorite } from "../favorite/favorite.js"
 export class LogReport extends Component {
 
     logReportSectionRef = this.useRef('log-report-section')
@@ -9,27 +10,33 @@ export class LogReport extends Component {
     durationUnexportedRef = this.useRef('duration-unexpored')
     logHistoryDateRangeTotalRef = this.useRef('log-history-total-range')
     logHistoryRef = this.useRef('log-history')
+    logHistoryTypeRef = this.useRef('log-history-type')
+    actionPinRef = this.useRef('action-pin-ref')
 
     constructor() {
         super(...arguments);
         this.subscribe('setGlobalData', this.setGlobalData.bind(this));
         this.subscribe('reloadHistory', this.reloadHistory.bind(this));
+        this.env.issueData.trackingMode = this.env.issueData.trackingMode || 'all';
+        this.env.issueData.lastDatetimeSelection = this.env.issueData.lastDatetimeSelection || [0, 0];
         this.secondToString = util.parseSecondToString(this.env.resource?.hrs_per_day || 8, this.env.resource?.days_per_week || 5)
     }
     onChangeRangeHistoryFilter(selectedDates, dateStr, instance){
         let from_unix = selectedDates[0].getTime()/1000;
         let to_unix = selectedDates[1].getTime()/1000;
-        this.loadHistory(from_unix, to_unix)
+        this.env.issueData.lastDatetimeSelection = [from_unix, to_unix];
+        this.loadHistory(from_unix, to_unix);
+        this.update('issueData', this.env.issueData);
     }
     reloadHistory(){
         this.loadHistory(this.unix[0], this.unix[1]+1)
     }
-    async loadHistory(from_unix=0, unix=0, refresh=false){
+    async loadHistory(from_unix=0, unix=0, refresh=false, keepTotal=false){
         if ((this.unix && this.unix[0]=== from_unix && this.unix[1] === unix) || refresh){
             return
         }
         this.unix = [from_unix, unix]
-        let response = (await this.do_invisible_request('GET', `${this.env.serverURL}/management/issue/work-log/history?from_unix=${from_unix}&unix=${unix}&jwt=${this.env.jwt}`));
+        let response = (await this.do_invisible_request('GET', `${this.env.serverURL}/management/issue/work-log/history?from_unix=${from_unix}&unix=${unix}&tracking=${this.env.issueData.trackingMode}&jwt=${this.env.jwt}`));
         let result = (await response.json());
         this.logHistoryRef.el.innerHTML = '';
         this.logHistoryDateRangeTotalRef.el.innerHTML = util.secondToHour(0)
@@ -106,8 +113,10 @@ export class LogReport extends Component {
                     }).mount(this.logHistoryRef.el)
                 }
             }
-            this.env.globalTotal = globalTotal;
-            this.env.exportedTotal = exportedTotal;
+            if (!keepTotal){
+                this.env.globalTotal = globalTotal;
+                this.env.exportedTotal = exportedTotal;
+            }
             this.setGlobalData();
             if (this.daterange){
                 this.daterange.setDate([new Date(this.unix[0]*1000), new Date(this.unix[1]*1000)]);
@@ -118,24 +127,49 @@ export class LogReport extends Component {
         this.logHistoryDateRangeTotalRef.el.innerHTML = util.secondToHour(this.env.globalTotal)
         this.durationUnexportedRef.el.innerHTML = util.secondToHour(this.env.globalTotal - this.env.exportedTotal)
     }
-
+    pinFilterChange(){
+        this.actionPinRef.el.classList.toggle('pinned');
+        this.env.issueData.pinFilter = !(this.env.issueData.pinFilter || false);
+        this.update('issueData', this.env.issueData);
+    }
+    logTypeChange(type='all'){
+        this.logHistoryTypeRef.el.classList.remove(this.env.issueData.trackingMode)
+        this.logHistoryTypeRef.el.classList.add(type)
+        this.env.issueData.trackingMode = type;
+        this.update('issueData', this.env.issueData);
+        this.loadHistory(this.unix[0], this.unix[1]+1, false, true)
+    }
     mounted() {
         let res = super.mounted();
-        this.loadHistory();
         let self = this;
+        let sUnix = 0, eUnix = 0;
+        if (Array.isArray(this.env.issueData.lastDatetimeSelection) && this.env.issueData.lastDatetimeSelection.length >= 2 && this.env.issueData.pinFilter){
+            sUnix = this.env.issueData.lastDatetimeSelection[0];
+            eUnix = this.env.issueData.lastDatetimeSelection[1];
+            this.actionPinRef.el.classList.add('pinned')
+        }
+        this.loadHistory(sUnix, eUnix);
         this.daterange = flatpickr(this.logHistoryDateRangeRef.el,{mode: "range", defaultDate: [new Date(), new Date()], altInput: true, altFormat: "M j, Y",
             onClose: self.onChangeRangeHistoryFilter.bind(self)
         });
+        this.actionPinRef.el.addEventListener('click', self.pinFilterChange.bind(self));
+        this.logHistoryDateRangeTotalRef.el.addEventListener('click', (e) => self.logTypeChange.bind(self)('all'))
+        this.durationUnexportedRef.el.addEventListener('click', (e) => self.logTypeChange.bind(self)('unexported'))
         return res
     }
     template = `
         <div class="log-report" l-ref="log-report-section">
             <div class="space-segment log-history-navigator"> 
-                <div class="log-history-navigator-action">
+                <div class="log-history-navigator-action" l-ref='log-history-type'>
                     <span class="filter-icon">
                         <svg class="svg-inline--fa fa-filter" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="filter" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M3.853 54.87C10.47 40.9 24.54 32 40 32H472C487.5 32 501.5 40.9 508.1 54.87C514.8 68.84 512.7 85.37 502.1 97.33L320 320.9V448C320 460.1 313.2 471.2 302.3 476.6C291.5 482 278.5 480.9 268.8 473.6L204.8 425.6C196.7 419.6 192 410.1 192 400V320.9L9.042 97.33C-.745 85.37-2.765 68.84 3.854 54.87L3.853 54.87z"></path></svg>
                     </span>
                     <input l-ref="history-date-range" class="log-history-navigator-input tm-form-control">
+                    <div class="pin-action" l-ref="action-pin-ref" title="Pin Filter">
+                        <span class="tm-icon-svg">
+                            <svg class="tm-svg-inline--fa" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-pin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M320 144C320 223.5 255.5 288 176 288C96.47 288 32 223.5 32 144C32 64.47 96.47 0 176 0C255.5 0 320 64.47 320 144zM192 64C192 55.16 184.8 48 176 48C122.1 48 80 90.98 80 144C80 152.8 87.16 160 96 160C104.8 160 112 152.8 112 144C112 108.7 140.7 80 176 80C184.8 80 192 72.84 192 64zM144 480V317.1C154.4 319 165.1 319.1 176 319.1C186.9 319.1 197.6 319 208 317.1V480C208 497.7 193.7 512 176 512C158.3 512 144 497.7 144 480z"></path></svg>
+                        </span>
+                    </div>
                     <div style="margin-left:auto"></div>
                     <div class="duration-description">
                         <span>    
@@ -144,8 +178,8 @@ export class LogReport extends Component {
                         <span l-ref="duration-unexpored">
                             00:00
                         </span>
-                        /
                     </div>
+                    /
                     <div class="total">
                         <div l-ref="log-history-total-range">00:00</div>
                     </div>
